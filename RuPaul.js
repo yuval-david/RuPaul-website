5203eaccc6d8ccdeaef5e55aa3805f8438373c50
yuval-david <yuvid0224@gmail.com>
2020-10-01 00:49:45 +0300

    first upload

---
+++ RuPaul.js
@@ -0,0 +1,22 @@
+/*****************************/
+/*** RuPaul Drag Race site ***/
+/******** Yuval David *******/
+/****************************/
+
+/****** Music ******/
+
+$("#heartIcon-music").click(() => {
+
+    $(this).toggleClass("glyphicon glyphicon-heart");
+
+
+});
+
+
+
+
+
+
+
+
+/*******************/
